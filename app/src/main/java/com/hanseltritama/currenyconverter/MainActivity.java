5bdf779bf5801fcd60ccdea61110db9c6ce0b47c
Hansel Tritama <hanseltritama@lmhanselt05283.home>
2018-09-26 02:23:29 -0700

    added design and logic

--- app/src/main/java/com/hanseltritama/currenyconverter/MainActivity.java
+++ app/src/main/java/com/hanseltritama/currenyconverter/MainActivity.java
@@ -2,8 +2,24 @@ package com.hanseltritama.currenyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void onConvertClicked(View view){
+
+        double idr = 0;
+        String outputText = "";
+        EditText numEditText = findViewById(R.id.numEditText);
+        idr = Double.parseDouble(numEditText.getText().toString())*15000;
+        outputText = NumberFormat.getIntegerInstance().format(idr);
+        Toast.makeText(this, "$" + numEditText.getText().toString()+" = Rp " + outputText, Toast.LENGTH_LONG).show();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
